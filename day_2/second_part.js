467f55879edaf32452ac8d8c62a5525ba6659f10
David Michel <david.michel2809@gmail.com>
2022-12-06 11:32:18 +0100

    feat: adding day_2 second part solution

---
+++ day_2/second_part.js
@@ -0,0 +1,29 @@
+import { formatedInput } from "./formatedInput.js";
+
+const points = {
+  scenarios: {
+    "X": {
+      "A": 3 + 0,
+      "B": 1 + 0,
+      "C": 2 + 0
+    },
+    "Y": {
+      "A": 1 + 3,
+      "B": 2 + 3,
+      "C": 3 + 3
+    },
+    "Z": {
+      "A": 2 + 6,
+      "B": 3 + 6,
+      "C": 1 + 6
+    }
+  }
+};
+
+const { scenarios } = points;
+
+let myScore = 0;
+
+for (const [opponentLetterShape, myLetterShape] of formatedInput) {
+  myScore += scenarios[myLetterShape][opponentLetterShape];
+}
