07fe61f3b5d08220cf6d6b5ba2ff9bf616c56216
David Michel <david.michel2809@gmail.com>
2022-12-06 15:30:16 +0100

    feat: adding day_4 second part solution

---
+++ day_4/second_part.js
@@ -0,0 +1,18 @@
+import { formatedInput } from "./formatedInput.js";
+import { testInput } from "./test_input.js";
+
+let containCount = 0;
+
+for (const [firstPair, secondPair] of formatedInput) {
+  if (
+    firstPair[0] >= secondPair[0] && firstPair[0] <= secondPair[1] ||
+    firstPair[1] >= secondPair[0] && firstPair[1] <= secondPair[1]
+    ||
+    secondPair[0] >= firstPair[0] && secondPair[0] <= firstPair[1] ||
+    secondPair[1] >= firstPair[0] && secondPair[1] <= firstPair[1]
+  ) {
+    containCount++;
+  }
+}
+
+debugger;
