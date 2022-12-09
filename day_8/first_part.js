def7b33fd7afcdc7c8c1de2a9df2a80773ccf805
David Michel <david.michel2809@gmail.com>
2022-12-09 15:00:06 +0100

    feat: adding day_8 part_1 solution

--- day_8/first_part.js
+++ day_8/first_part.js
@@ -1,2 +1,93 @@
 import { parsedInput } from "./input/parsedInput.js";
 import { parsedInputTest } from "./input/parsedInputTest.js";
+
+function getVisibleTrees(input) {
+  let visibleTreeCount = 0;
+  // helps to visualize which tree are visible
+  const visibleTrees = {};
+
+  for (let y = 0; y < input.length; y++) {
+    const currentLine = input[y];
+
+    for (let x = 0; x < currentLine.length; x++) {
+      const currentTreeHeight = Number(currentLine[x]);
+
+      // if tree is on a edge, add it to visible trees
+      if (
+        y === 0 ||
+        y === input.length - 1 ||
+        x === 0 ||
+        x === currentLine.length - 1
+      ) {
+        visibleTreeCount++;
+        // helps to visualize which tree are visible
+        visibleTrees[`${y}, ${x}`] = currentTreeHeight;
+      }
+      else {
+        // check at the left of current tree if any tree is bigger
+        let hasFoundBiggerTreeInTheLeft = false;
+
+        for (let leftX = x - 1; leftX >= 0; leftX--) {
+          const currentLeftXHeight = Number(currentLine[leftX]);
+
+          if (currentLeftXHeight >= currentTreeHeight) {
+            hasFoundBiggerTreeInTheLeft = true;
+            break;
+          }
+        }
+
+        //check at the right of current tree is any tree is bigger
+        let hasFoundBiggerTreeInTheRight = false;
+
+        for (let rightX = x + 1; rightX < currentLine.length; rightX++) {
+          const currentRightXHeight = Number(currentLine[rightX]);
+
+          if (currentRightXHeight >= currentTreeHeight) {
+            hasFoundBiggerTreeInTheRight = true;
+            break;
+          }
+        }
+
+        // check above the current tree if any tree is bigger
+        let hasFoundBiggerTreeAbove = false;
+
+        for (let topX = y - 1; topX >= 0; topX--) {
+          const currentTopXHeight = Number(input[topX][x]);
+
+          if (currentTopXHeight >= currentTreeHeight) {
+            hasFoundBiggerTreeAbove = true;
+            break;
+          }
+        }
+
+        // check below the current tree if any tree is bigger
+        let hasFoundBiggerTreeBelow = false;
+
+        for (let bottomX = y + 1; bottomX < input.length; bottomX++) {
+          const currentBottomXHeight = Number(input[bottomX][x]);
+
+          if (currentBottomXHeight >= currentTreeHeight) {
+            hasFoundBiggerTreeBelow = true;
+            break;
+          }
+        }
+
+        if (
+          hasFoundBiggerTreeInTheLeft === false ||
+          hasFoundBiggerTreeInTheRight === false ||
+          hasFoundBiggerTreeAbove === false ||
+          hasFoundBiggerTreeBelow === false
+        ) {
+          visibleTrees[`${y}, ${x}`] = currentTreeHeight;
+          visibleTreeCount++;
+        }
+      }
+    }
+  }
+
+  return visibleTreeCount;
+}
+
+
+const visibleCount = getVisibleTrees(parsedInput);
+debugger;
