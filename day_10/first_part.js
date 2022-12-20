9923c855a3cfaa166d1aa45d7ef34bc308a727e9
David Michel <david.michel2809@gmail.com>
2022-12-20 13:15:39 +0100

    feat: add day_10 first_part solution

--- day_10/first_part.js
+++ day_10/first_part.js
@@ -1,1 +1,32 @@
 import { parsedInput, parsedTestInput } from "./input/parsedInputs.js";
+
+function getSignalStrength(input) {
+  let x = 1;
+  let cycleCount = 0;
+  let signalStrength = 0;
+
+  function checkCycleCount(xValue) {
+    if (cycleCount === 20 || cycleCount > 20 && (cycleCount - 20) % 40 === 0) {
+      signalStrength += cycleCount * xValue;
+    }
+  }
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i][0] === "noop") {
+      cycleCount++;
+    } else {
+      for (let j = 0; j < 2; j++) {
+        cycleCount++;
+        checkCycleCount(x);
+      }
+      x += parseInt(input[i][1], 10);
+      continue;
+    }
+
+    checkCycleCount(x);
+  }
+
+  return signalStrength;
+}
+
+const signalStrength = getSignalStrength(parsedInput);
